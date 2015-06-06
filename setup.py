5a5e1f4276c713db1e40f19e922f99c06b10b08d
Ali-Akber Saifee <ali@indydevs.org>
2015-06-06 18:56:41 +0800

    Tweak setup.py to support dependency link

--- setup.py
+++ setup.py
@@ -8,11 +8,17 @@ __email__ = "ali@indydevs.org"
 __copyright__ = "Copyright 2015, Ali-Akber Saifee"
 
 from setuptools import setup, find_packages
-import os
+import os, re
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
-REQUIREMENTS = filter(None, open(
+egg = re.compile("\#egg\=(.*?)$")
+requirements = filter(None, open(
     os.path.join(this_dir, 'requirements', 'main.txt')).read().splitlines())
+REQUIREMENTS = [req for req in requirements if not req.startswith('-e')]
+DEPENDENCY_LINKS = [req.replace('-e ','') for req in requirements if req.startswith('-e')]
+REQUIREMENTS.extend([egg.findall(req)[0] for req in requirements if req.startswith('-e')])
+
+print REQUIREMENTS, DEPENDENCY_LINKS
 
 import versioneer
 
@@ -31,6 +37,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     install_requires=REQUIREMENTS,
+    dependency_links=DEPENDENCY_LINKS,
     classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
     description='Rate limiting utilities',
     long_description=open('README.rst').read() + open('HISTORY.rst').read(),
